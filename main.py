4b847951a7ac69cadbe4dd81f0c39d7bd9abb43d
ArkinEarle <124639509+ArkinEarle@users.noreply.github.com>
2024-06-23 21:06:01 +0000

    Jun 23, 2024, 9:06 PM

--- main.py
+++ main.py
@@ -5,9 +5,21 @@ Created by: NAME
 Date: DATE
 """
 
-# Enter your code here
-name: str = input("Enter your name: ")
-if name:
-    print(f"Hello, {name}!")
-else:
-    print("Error getting name.")
+from random import shuffle
+import pandas as PD
+import remi.gui as GUI
+from remi import App, start
+
+
+class BlackJackApp(App):
+    def __init__(self, *args) -> None:
+        import os
+        abs_path: str = os.path.abspath(__file__)
+        dir_name: str = os.path.dirname(abs_path)
+        res_path: str = os.path.join(dir_name, "res")
+
+
+    def main(self) -> GUI.Widget:
+        return GUI.HBox
+
+start(BlackJackApp)
