08b0772ac65225cb7f5ab2087c2c6b16eb5c1092
ArkinEarle <124639509+ArkinEarle@users.noreply.github.com>
2024-08-22 14:29:08 +1200

    implement bet option

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
-import pygame
 import random
+import slider
+import pygame
 import os
+
+
+
+class Button:
+    def __init__(self, x, y, width, height, text, color=(208, 25, 32), hover_color=(135, 17, 20), font_size=30):
+        self.rect = pygame.Rect(x, y, width+20, height)
+        self.color = color
+        self.hover_color = hover_color
+        self.font = pygame.font.Font('Grand9K.ttf', font_size)
+        self.text = text
+        self.text_surface = self.font.render(text, True, (255,255,255), (0,0,0))
+        self.text_surface.set_colorkey((0,0,0))
+        self.text_rect = pygame.Rect(0,0, width, height)
+        self.text_rect.center = self.rect.center
+
+    def draw(self, screen):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(mouse_pos):
+            pygame.draw.rect(screen, self.hover_color, self.rect)
+        else:
+            pygame.draw.rect(screen, self.color, self.rect)
+        screen.blit(self.text_surface, self.text_rect)
+
+    def is_clicked(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            if self.rect.collidepoint(event.pos):
+                return True
+        return False
+
+    def update_text(self, new_text):
+        self.text = new_text
+        self.text_surface = self.font.render(new_text, True, (255,255,255))
+        self.text_rect = self.text_surface.get_rect(center=self.rect.center)
 
 
 class Card:
@@ -42,41 +76,6 @@ GRAY = (175, 175, 175)
 
 
 
-class Button:
-    def __init__(self, x, y, width, height, text, color=(208, 25, 32), hover_color=(135, 17, 20), font_size=30):
-        self.rect = pygame.Rect(x, y, width+20, height)
-        self.color = color
-        self.hover_color = hover_color
-        self.font = pygame.font.Font('Grand9K.ttf', font_size)
-        self.text = text
-        self.text_surface = self.font.render(text, True, WHITE, BLACK)
-        self.text_surface.set_colorkey((0,0,0))
-    #    self.text_surface = pygame.Surface(x,y)
-    #    self.text_rect.center = self.rect.center
-
-
-    def draw(self, screen):
-        mouse_pos = pygame.mouse.get_pos()
-        if self.rect.collidepoint(mouse_pos):
-            pygame.draw.rect(screen, self.hover_color, self.rect)
-        else:
-            pygame.draw.rect(screen, self.color, self.rect)
-        screen.blit(self.text_surface, self.rect)
-
-
-    def is_clicked(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            if self.rect.collidepoint(event.pos):
-                return True
-        return False
-
-
-    def update_text(self, new_text):
-        self.text = new_text
-        self.text_surface = self.font.render(new_text, True, BLACK)
-        self.text_rect = self.text_surface.get_rect(center=self.rect.center)
-
-
 
 
 class ScoreText:
@@ -109,18 +108,36 @@ HIDDEN_NMB = 1
 
 class Main:
     def __init__(self):
+        self.event = None
         self.restarted = False
+        self.bet_button_isdrawn = False
+
+
         pygame.init()
         self.screenDim = (SCREEN_WIDTH, SCREEN_HEIGHT)
         self.screen = pygame.display.set_mode(self.screenDim)
-    #    self.background = pygame.transform.scale_by(pygame.image.load('black_jack_table.jpg').convert(),1)
-        self.background = pygame.image.load('black_jack_table.jpg').convert()
+        self.background = pygame.transform.scale_by(pygame.image.load('black_jack_table.jpg').convert(),1)
         self.running = False
+        self.bank = 10000
+        self.minimum_bet = 10
+        self.current_bet = 0
+        self.bet_subtracted_yet = False
+        self.font = pygame.font.Font('Grand9K.ttf', 30)
 
 
+        self.account_balance = self.font.render(f'${self.bank}', True,
+                                                (255, 255, 255), (208, 25, 32))
+        self.account_balance_rect = self.account_balance.get_rect()
+        self.account_balance_rect.center = (100,40)
+        self.bet_slider = slider.slider([640, 540], self.screen, [10, 40], (90, 90, 90), [640, 540],
+                                        [400, 20], self.bank, self.minimum_bet, (255, 255, 255))
+        self.current_bet_text = self.font.render(f'${round(self.current_bet/10)*10}', True,
+                                                            (255, 255, 255), (208, 25, 32))
+        self.current_bet_text_rect = self.current_bet_text.get_rect()
+        self.current_bet_text_rect.center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/4)
         # Player buttons
-        self.restart_button = Button(x=SCREEN_WIDTH / 2 -230/2 , y=1.25*SCREEN_HEIGHT/3, width=230, height=75, text="Restart", font_size=50)
-        self.start_button = Button(x=SCREEN_WIDTH / 2 -180/2 , y=1.25*SCREEN_HEIGHT/3, width=180, height=75, text="Start", font_size=50)
+        self.restart_button = Button(x=SCREEN_WIDTH / 2 - 230 / 2, y=1.25 * SCREEN_HEIGHT / 3, width=230, height=75, text="Restart", font_size=50)
+        self.bet_button = Button(x=SCREEN_WIDTH / 2 - 240 / 2, y=1.25 * SCREEN_HEIGHT / 3, width=240, height=75, text="Place bet", font_size=50)
         self.hit_button = Button(
             x=SCREEN_WIDTH / 2 -100,
             y=620,
@@ -140,7 +157,7 @@ class Main:
         # Score texts
         self.p_score = ScoreText(
             x=SCREEN_WIDTH / 2 - 50,
-            y=580,
+            y=550,
             width=100,
             height=50,
             text="0"
@@ -152,6 +169,15 @@ class Main:
             height=50,
             text="0"
         )
+        self.event_text = ScoreText(
+            x=SCREEN_WIDTH / 2 - 50,
+            y=100,
+            width=100,
+            height=50,
+            text="None"
+        )
+
+
         self.p_total = 0
         self.d_total = 0
 
@@ -182,13 +208,16 @@ class Main:
 
         self.p_turn = True
         self.hide_card = True
-        self.started = False
-        self.player_win = None
+        self.bet = False
         self.hand_finsihed = False
+        self.started = False
+
+
     def load_card_images(self):
         card_images = {}
         for suit in Deck.suits:
             for rank in Deck.ranks:
+                
                 dir_path = os.path.dirname(os.path.realpath(__file__))
                 root_dir = dir_path + r"\Sprites\KIN's_Playing_Cards"
 
@@ -205,11 +234,56 @@ class Main:
 
     def poll(self):
         for event in pygame.event.get():
-            if self.start_button.is_clicked(event):
+            if self.bet_button.is_clicked(event) and not self.started and self.bet_button_isdrawn:
+                self.bet_button_isdrawn = False
                 self.started = True
+                # print("self.started = True", self.started)
+                self.bet = True
+
+
+
+                if self.event != None:
+                    self.old_event = self.event
+                # events
+
+                # first: standonly
+                # must stand
+
+                # second: no_risk
+                # bet +100 every time you hit
+
+                # third: 21or0
+                # 21 or nothing
+
+
+                random_id = random.random()
+                if 0 <= random_id < 0.34:
+                    print("0 - .33")
+                    self.event = "standonly"
+                elif .34 <= random_id < 0.67:
+                    print(".34 - .66")
+                    self.event = "no_risk"
+                else:
+                    print(".67 - 1")
+                    self.event = "21or0"
+                print(random_id)
+                print(self.event)
+
+
+                if not self.bet_subtracted_yet and self.event != "no_risk":
+                        self.bank -= self.current_bet
+                        self.bet_subtracted_yet = True
+
             self.restarted = False
-            if self.restart_button.is_clicked(event):
+
+            # print("self.started = ", self.started)
+            if self.restart_button.is_clicked(event) and not self.bet_button_isdrawn:
+                self.event_text.update_text("Current Event: None")
+                self.started = False
+                # print("self.started = False", self.started)
                 self.restarted = True
+                self.d_shoe = Deck(nmb_decks=4)
+
 
 
             if event.type == pygame.QUIT:
@@ -218,11 +292,11 @@ class Main:
                 if event.key == pygame.K_ESCAPE:
                     self.running = False
             if self.p_total < 21:
-                if self.hit_button.is_clicked(event):
+                if self.hit_button.is_clicked(event) and self.event != "standonly" and not self.bet_button_isdrawn:
                     self.card_drawn("player")
 
 
-                elif self.stand_button.is_clicked(event):
+                elif self.stand_button.is_clicked(event) and not self.bet_button_isdrawn:
                     self.p_turn = False
                     if self.d_total > 21:
                         self.d_score.update_text("BUSTED")
@@ -240,24 +314,58 @@ class Main:
                     self.d_score.update_text("BLACKJACK")
                 else:
                     self.d_score.update_text(f"{self.d_total}")
+
+
+
             if ((not self.p_turn and self.d_total >= 17) or (self.p_total>21)) and self.restarted:
-                print(self.restarted)
                 self.previous_cards_p.clear()
                 self.previous_cards_d.clear()
+                if self.p_total < self.d_total <= 21 or self.p_total > 21:
+                    print(self.bank)
+                    self.bet_slider = slider.slider([640, 540], self.screen, [10, 40], (90, 90, 90), [640, 540],
+                                                    [400, 20], self.bank, self.minimum_bet, (255, 255, 255))
+                    if self.bank == 0:
+                        pygame.quit()
+
+
+                elif self.p_total == self.d_total:
+                    
+
+                    if (self.event == "21or0"):
+                        if (self.p_total == 21):
+                            self.bank += self.current_bet
+                            
+                    else:
+                        if event != "no_risk":
+                            self.bank += self.current_bet
+
+
+                elif self.p_total == 21:
+                    self.bank += 2.5*self.current_bet
+
+
+                else:
+                    if (self.event == "21or0"):
+                        if (self.p_total == 21):
+                            self.bank += self.current_bet*2
+                    else:
+                        self.bank += self.current_bet*2
+                    
+                    print(self.bank)
+                    self.bet_slider = slider.slider([640, 540], self.screen, [10, 40], (90, 90, 90), [640, 540],
+                                                    [400, 20], self.bank, self.minimum_bet, (255, 255, 255))
+
+
+                self.event = "None"
                 if self.d_total > 0:
                     self.d_total = 0
                 if self.p_total>0:
                     self.p_total = 0
-                self.player_win = None
                 self.p_turn = True
+                self.bet_subtracted_yet = False
+                self.bet =False
                 self.restarted = False
                 self.setup()
-                if self.p_total<self.d_total and self.d_total > 21 or self.p_total > 21:
-                    self.player_win = False
-                else:
-                    self.player_win = True
-
-
 
 
     def update(self, dt):  # idk might get rid of
@@ -265,8 +373,12 @@ class Main:
 
 
     def draw(self):
+
+
         if self.p_turn:
-            self.hit_button.draw(self.screen)
+            if self.event != "standonly":
+                self.hit_button.draw(self.screen)
+
             self.stand_button.draw(self.screen)
 
 
@@ -280,10 +392,18 @@ class Main:
             pc_offset += 25
 
 
+
+
+
+
+
+
         # self.screen.blit(
         #     self.card_back, (SCREEN_WIDTH / 2 - 20 + dc_offset, 210)
         # )
         # dc_offset += 25
+
+
 
 
         for i, card in enumerate(self.previous_cards_d):
@@ -311,6 +431,8 @@ class Main:
         self.d_score.draw(self.screen)
 
 
+
+
     def setup(self):
         self.previous_cards_p.clear()
         self.previous_cards_d.clear()
@@ -323,17 +445,24 @@ class Main:
 
 
     def run(self):
-
-
         self.setup()
         self.running = True
         clock = pygame.time.Clock()
+
         while self.running:
+
+
+            
+
             dt = clock.tick(40) / 1000.0
             self.screen.blit(self.background, (0,0))
-        #    self.screen.blit((0,0))
             self.poll()
-            if self.started:
+
+
+            self.event_text.update_text(f"Current Event: {self.event if self.event else 'None'}")
+            self.event_text.draw(self.screen)
+
+            if self.bet:
 
 
                 self.draw()
@@ -341,16 +470,20 @@ class Main:
                     while self.d_total < 17:
                         self.card_drawn("dealer")
             else:
-                self.start_button.draw(self.screen)
+                self.bet_button.draw(self.screen)
+                self.bet_button_isdrawn = True
+                self.current_bet = round(self.bet_slider.slider_update(self.screen, (255,255,255))/10)*10
+                self.current_bet_text = self.font.render(f'${round(self.current_bet / 10) * 10}', True,
+                                                        (255, 255, 255), (208, 25, 32))
+                self.screen.blit(self.current_bet_text, self.current_bet_text_rect)
             if self.p_turn is False and self.d_total >= 17:
 
 
                 self.restart_button.draw(self.screen)
-
-
+            self.account_balance = self.font.render(f'${self.bank}', True,
+                                                    (255, 255, 255), (208, 25, 32))
+            self.screen.blit(self.account_balance,self.account_balance_rect)
             self.update(dt)
-
-
             pygame.display.flip()
 
 
@@ -536,4 +669,3 @@ if __name__ == '__main__':
     print("starting...")
     main.run()
     print("shutting down...")
-
